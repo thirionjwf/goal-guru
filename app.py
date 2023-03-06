ca2e310a5b4a2d7acc70074d0e98f853f0e61ff2
Derik Thirion <thirionjwf@gmail.com>
2023-03-06 06:09:47 +0200

    Refactored code.

--- app.py
+++ app.py
@@ -3,11 +3,8 @@ import gradio as gr
 import openai
 import pandas as pd
 
-cfg = config.Config(".env")
-openai.api_key = cfg["OPEN_AI_API_KEY"]
 
-
-def get_goal(title, objective):
+def get_goal(title: str, objective: str) -> str:
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
@@ -15,21 +12,8 @@ def get_goal(title, objective):
                 "role": "system",
                 "content": f"""
                     You are an employee, working as a {title} in an insurance company.
-                    """,
-            },
-            {
-                "role": "system",
-                "content": """
                     You're manager has given you the following high-level objective which you need to use to create a SMART goal for 2023:
-                    """,
-            },
-            {
-                "role": "system",
-                "content": f"{objective}",
-            },
-            {
-                "role": "system",
-                "content": """
+                    {objective}
                     You need to write SMART goals broken up in clear and specific goals, an approach to measure the outcome (and in addition suggest a scale of 1-5 for measurement.
                     The principle of the measurement scale is that a 1 indicates under-performing significantly, 2 sometimes under-performing, 3 is achieving what has been asked, 4 is significantly outperforming, and 5 indicates that the individual redefined what a score of 3 should be.
                     The measurement scale needs to be adapted and made relevant to the specific SMART goal.
@@ -45,23 +29,35 @@ def get_goal(title, objective):
     return response.choices[0]["message"]["content"]
 
 
-company_objectives = pd.read_csv("company_objectives.csv")
-print(company_objectives.info())
+def get_interface():
+    with gr.Blocks() as iface:
+        job_desc = gr.Textbox(placeholder="Your job title", label="Job description")
+        objective = gr.Textbox(
+            lines=15,
+            placeholder="Enter high-level objectives here, one per line, e.g.:\n1. Increase sales of new contracts by 10%, to boost the revenue of the company.\n2. Retain high-value customers by reducing churn by 25%.",
+            label="Objective",
+        )
+        output_goal = gr.Textbox(label="Goal")
 
-with gr.Blocks() as iface:
-    job_desc = gr.Textbox(placeholder="Your job title", label="Job description")
-    objective = gr.Textbox(
-        lines=15,
-        placeholder="Enter high-level objectives here, one per line, e.g.:\n1. Increase sales of new contracts by 10%, to boost the revenue of the company.\n2. Retain high-value customers by reducing churn by 25%.",
-        label="Objective",
-    )
-    output_goal = gr.Textbox(label="Goal")
+        get_btn = gr.Button("Get my goals")
+        get_btn.click(
+            fn=get_goal,
+            inputs=[job_desc, objective],
+            outputs=output_goal,
+        )
 
-    get_btn = gr.Button("Get my goals")
-    get_btn.click(
-        fn=get_goal,
-        inputs=[job_desc, objective],
-        outputs=output_goal,
-    )
+    return iface
 
-iface.launch()
+
+def main():
+    cfg = config.Config(".env")
+    openai.api_key = cfg["OPEN_AI_API_KEY"]
+
+    company_objectives = pd.read_csv("company_objectives.csv")
+    print(company_objectives.info())
+
+    get_interface().launch(share=False)
+    return
+
+
+main()
